5d6eab29ffe93841e71a2e24c3109ea5f0f7200f
lexhui1999 <lexhui1999@gmail.com>
2022-06-09 04:24:12 -0400

    Implemented timeout functionality

--- packets.py
+++ packets.py
@@ -2,54 +2,55 @@ import struct
 
 icmp_struct = "bbHHh"
 
-class icmp():	
+class ICMP_Packet():
 
-	def __init__(self, type=8, code=0, checksum=0, id=1, seq=1):
-		self.type = type
-		self.code = code
-		self.checksum = checksum
-		self.id = id
-		self.seq = seq
-		self.data = None
-		self.raw = None
-		self.pack()
+    def __init__(self, type=8, code=0, id=1, seq=1):
+        self.type = type
+        self.code = code
+        self.checksum = 0
+        self.id = id
+        self.seq = seq
+        self.data = None
+        self.raw = None
+        self.pack()
 
-	def pack(self):
-		self.raw = struct.pack(icmp_struct,
-			self.type,
-			self.code,
-			self.checksum,
-			self.id,
-			self.seq)
+    def pack(self):
+        self.raw = struct.pack(icmp_struct,
+                               self.type,
+                               self.code,
+                               self.checksum,
+                               self.id,
+                               self.seq)
 
-		self.checksum = self.checksum_calc(self.raw)
+        self.checksum = self.checksum_calc(self.raw)
 
-		self.raw = struct.pack(icmp_struct,
-			self.type,
-			self.code,
-			self.checksum,
-			self.id,
-			self.seq)
+        self.raw = struct.pack(icmp_struct,
+                               self.type,
+                               self.code,
+                               self.checksum,
+                               self.id,
+                               self.seq)
 
-	def checksum_calc(self, msg):
-		s = 0       # Binary Sum
+    @staticmethod
+    def checksum_calc(msg):
+        s = 0  # Binary Sum
 
         # loop taking 2 characters at a time
-		for i in range(0, len(msg), 2):
+        for i in range(0, len(msg), 2):
+            a = msg[i]
+            b = msg[i + 1]
+            s = s + (a + (b << 8))
 
-			a = msg[i]
-			b = msg[i+1]
-			s = s + (a+(b << 8))
-            
-        
-		# One's Complement
-		s = s + (s >> 16)
-		s = ~s & 0xffff
+        # One's Complement
+        s = s + (s >> 16)
+        s = ~s & 0xffff
 
-		return s
+        return s
+
 
 def main():
-	packet = icmp()
+    packet = ICMP_Packet()
+
 
 if __name__ == "__main__":
-	main()
+    main()
